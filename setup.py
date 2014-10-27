cd432a775dbb5f529d8996b8663ab4a9ab9d7145
Wojciech Reszelewski <wreszelewski@gmail.com>
2014-10-27 21:35:46 +0100

    Form of a library

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nsga2',
+      version='0.1',
+      description='NSGA-II Algorithm implementation',
+      author='Wojciech Reszelewski, Kamil Mielnik',
+      author_email='wreszelewski@gmail.com',
+      url = "https://github.com/wreszelewski/nsga2",
+      packages=['nsga2', 'nsga2.problems'],
+     )
